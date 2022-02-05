183efe031b4fda88c298dbd6cf46177ba411a96d
Rodrigo Rafa <93699810+Rorafa21@users.noreply.github.com>
2022-02-05 13:46:37 -0300

    Update Exercicio31CeV.py
    sem aspas

--- Exercicio31CeV.py
+++ Exercicio31CeV.py
@@ -1,6 +1,6 @@
 #Calculando frete de uma viagem com desconto acima de 200km
 
-km = float(input('\033[31mQuantos KM até seu destino ? \033[m))
+km = float(input('\033[31mQuantos KM até seu destino ? \033[m'))
 if km <= 200:
     n1 = km * 0.50
     print('sua viagem custará \033[1:35mR${}\033[m'.format(n1))
